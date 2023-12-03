fe578b71850078e622946d6a142bc3d98f5f16d5
tyrellbaker-blip <91085471+tyrellbaker-blip@users.noreply.github.com>
2023-12-03 09:25:36 -0800

    Removed the back functionality because the app is only 3 screens, so who gives a shit, fixed the back button, changed the background colors.

--- app/src/main/java/com/example/crossoverconvos/MainActivity.java
+++ app/src/main/java/com/example/crossoverconvos/MainActivity.java
@@ -2,22 +2,21 @@ package com.example.crossoverconvos;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
 import com.google.firebase.auth.FirebaseAuthInvalidUserException;
-import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
     private EditText editTextEmail, editTextPassword;
-    private Button buttonLogin, buttonLogout; // Added logout button
-    private FirebaseAuth auth; // Firebase Auth instance
+    private Button buttonLogin;
+    private FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,33 +27,11 @@ public class MainActivity extends AppCompatActivity {
         editTextEmail = findViewById(R.id.editTextEmail);
         editTextPassword = findViewById(R.id.editTextPassword);
         buttonLogin = findViewById(R.id.buttonLogin);
-        buttonLogout = findViewById(R.id.buttonLogout);
 
         buttonLogin.setOnClickListener(v -> loginUser());
-        buttonLogout.setOnClickListener(view -> {
-            FirebaseAuth.getInstance().signOut();
-            updateUIForLoggedOutUser();
-        });
 
         // Check authentication state when the activity is created
         checkAuthenticationState();
-        updateUIBasedOnLoginStatus();
-    }
-
-    private void updateUIBasedOnLoginStatus() {
-        if (FirebaseAuth.getInstance().getCurrentUser() != null) {
-            // User is logged in
-            buttonLogout.setVisibility(View.VISIBLE);
-            // Show main content
-        } else {
-            // User is not logged in
-            buttonLogout.setVisibility(View.GONE);
-            // Show login interface
-        }
-    }
-
-    private void updateUIForLoggedOutUser() {
-        buttonLogout.setVisibility(View.GONE);
     }
 
     private void loginUser() {
@@ -69,11 +46,9 @@ public class MainActivity extends AppCompatActivity {
         FirebaseAuth.getInstance().signInWithEmailAndPassword(email, password)
                 .addOnCompleteListener(this, task -> {
                     if (task.isSuccessful()) {
-                        // Directly go to UserFeedActivity without checking email verification
                         startActivity(new Intent(MainActivity.this, UserFeedActivity.class));
                         finish();
                     } else {
-                        // Handle login failure
                         if (task.getException() instanceof FirebaseAuthInvalidUserException) {
                             Toast.makeText(MainActivity.this, "User not found.", Toast.LENGTH_LONG).show();
                         } else if (task.getException() instanceof FirebaseAuthInvalidCredentialsException) {
@@ -85,37 +60,10 @@ public class MainActivity extends AppCompatActivity {
                     }
                 });
     }
-    private void logoutUser() {
-        auth.signOut();
-        resetToLoginScreen();
-    }
 
-    private void resetToLoginScreen() {
-        // Reset UI elements to initial state for login
-        editTextEmail.setText("");
-        editTextPassword.setText("");
-}
     @Override
     protected void onStart() {
         super.onStart();
-        // Check if user is signed in (non-null) and update UI accordingly.
-        if (auth.getCurrentUser() != null) {
-            // User is signed in, navigate to UserFeedActivity or appropriate screen
-            navigateToUserFeed();
-        } else {
-            // User is not signed in, stay on this login screen
-            resetToLoginScreen();
-        }
-    }
-    private void navigateToUserFeed() {
-        Intent intent = new Intent(MainActivity.this, UserFeedActivity.class);
-        startActivity(intent);
-        finish();
-    }
-    @Override
-    protected void onResume() {
-        super.onResume();
-        // Check authentication state when the activity resumes
         checkAuthenticationState();
     }
 
@@ -128,5 +76,4 @@ public class MainActivity extends AppCompatActivity {
             Toast.makeText(this, "User is not logged in", Toast.LENGTH_LONG).show();
         }
     }
-
 }
--- app/src/main/java/com/example/crossoverconvos/UserFeedActivity.java
+++ app/src/main/java/com/example/crossoverconvos/UserFeedActivity.java
@@ -1,13 +1,17 @@
 package com.example.crossoverconvos;
+
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
+import android.os.Handler;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
@@ -22,24 +26,75 @@ public class UserFeedActivity extends AppCompatActivity {
     private List<Post> postList;
     private EditText editTextPost;
     private Button buttonPost;
+    private Handler inactivityHandler = new Handler();
+    private FloatingActionButton fabLogout;
+    private Runnable inactivityRunnable = new Runnable() {
+        @Override
+        public void run() {
+            FirebaseAuth.getInstance().signOut();
+            startActivity(new Intent(UserFeedActivity.this, MainActivity.class));
+            finish();
+        }
+    };
+    private static final long INACTIVITY_TIMEOUT = 10 * 60 * 1000; // 10 minutes
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_feed);
-
         recyclerViewPosts = findViewById(R.id.recyclerViewPosts);
         editTextPost = findViewById(R.id.editTextPost);
         buttonPost = findViewById(R.id.buttonPost);
-
         recyclerViewPosts.setLayoutManager(new LinearLayoutManager(this));
         postList = new ArrayList<>();
         postAdapter = new PostAdapter(this, postList);
         recyclerViewPosts.setAdapter(postAdapter);
-
         buttonPost.setOnClickListener(view -> submitPost());
+        fabLogout = findViewById(R.id.fabLogout);
+        fabLogout.setOnClickListener(view -> {
+            FirebaseAuth.getInstance().signOut();
+            startActivity(new Intent(UserFeedActivity.this, MainActivity.class));
+            finish();
+        });
 
-        fetchPosts();
+        fetchPosts(); // Fetch posts when the activity is created
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        resetInactivityTimer();
+    }
+
+    @Override
+    public void onUserInteraction() {
+        super.onUserInteraction();
+        resetInactivityTimer();
+    }
+
+    private void resetInactivityTimer() {
+        inactivityHandler.removeCallbacks(inactivityRunnable);
+        inactivityHandler.postDelayed(inactivityRunnable, INACTIVITY_TIMEOUT);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.menu_logout) {
+            // Handle logout button action
+            FirebaseAuth.getInstance().signOut();
+            startActivity(new Intent(this, MainActivity.class));
+            finish();
+            return true;
+        }
+        // Handle other menu items if necessary
+        return super.onOptionsItemSelected(item);
     }
 
     private void submitPost() {
@@ -60,9 +115,9 @@ public class UserFeedActivity extends AppCompatActivity {
                 .add(newPost)
                 .addOnSuccessListener(documentReference -> {
                     newPost.setPostId(documentReference.getId());
-                    postList.add(0, newPost); // Add at the top of the list
+                    postList.add(0, newPost);
                     postAdapter.notifyItemInserted(0);
-                    editTextPost.setText(""); // Clear the input field
+                    editTextPost.setText("");
                 })
                 .addOnFailureListener(e -> Toast.makeText(this, "Failed to add post", Toast.LENGTH_SHORT).show());
     }
@@ -79,7 +134,7 @@ public class UserFeedActivity extends AppCompatActivity {
                     if (queryDocumentSnapshots != null) {
                         for (DocumentSnapshot snapshot : queryDocumentSnapshots.getDocuments()) {
                             Post post = snapshot.toObject(Post.class);
-                            post.setPostId(snapshot.getId()); // Set the post ID
+                            post.setPostId(snapshot.getId());
                             postList.add(post);
                         }
                         postAdapter.notifyDataSetChanged();
